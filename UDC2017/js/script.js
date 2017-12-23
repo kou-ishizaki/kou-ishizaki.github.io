fe9ba09486dd255076474427592d13fae9578bd9
14H-02390 <kou06ishi16@gmail.com>
2017-12-23 18:09:44 +0900

    no message

--- UDC2017/js/script.js
+++ UDC2017/js/script.js
@@ -1,17 +1,14 @@
 //地図表示の設定
-/*var map = L.map('map',{
+var map = L.map('map',{
     center: [34.393073, 132.452091],
     zoom: 18,
 });
-*/
-
-var map = L.map('map').setView([34.393073, 132.452091], 18);
 
 
 //タイルレイヤの表示
 L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
-        attribution: '&copy; <a href='http://www.gsi.go.jp/kikakuchousei/kikakuchousei40182.html' target='_blank'>国土地理院</a>',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
     }
 ).addTo(map);
