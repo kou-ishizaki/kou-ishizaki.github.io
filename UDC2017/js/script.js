08c8c222df37fee664954cb3f2e8f14908bdd7a2
14H-02390 <kou06ishi16@gmail.com>
2017-12-24 15:16:43 +0900

    元に戻す

--- UDC2017/js/script.js
+++ UDC2017/js/script.js
@@ -1,4 +1,4 @@
-var childlencafeteria = L.layerGroup();
+/*var childlencafeteria = L.layerGroup();
 
 	L.marker([34.392499, 132.475163]).bindPopup('青い鳥').addTo(childlencafeteria),
 	L.marker([34.361024, 132.463756]).bindPopup('くるり食堂').addTo(childlencafeteria);
@@ -13,15 +13,17 @@ var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</
 var grayscale   = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
     streets  = L.tileLayer(mbUrl, {id: 'mapbox.streets',   attribution: mbAttr});
 
+*/
 
 //地図表示の設定
 var map = L.map('map',{
     center: [34.393073, 132.452091],
     zoom: 16,
-    layers: [grayscale, childlencafeteria]
+//    layers: [grayscale, childlencafeteria]
 });
 
 
+/*
 var baseLayers = {
     "Grayscale": grayscale,
     "Streets": streets
@@ -39,11 +41,12 @@ L.control.layers(baseLayers, overlays).addTo(map);
 //    CC2 = L.marker([34.361024, 132.463756]).bindPopup('くるり食堂');
 //map = L.layerGroup([CC1, CC2]);
 
+*/
 
 //タイルレイヤの表示
-//L.tileLayer(
-//    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-//    {
-//        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-//    }
-//).addTo(map);
+L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+    }
+).addTo(map);
