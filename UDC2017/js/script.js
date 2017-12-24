c3cb761b50179c1baeccf8b38300c5d2d0e96c2e
14H-02390 <kou06ishi16@gmail.com>
2017-12-24 15:23:36 +0900

    no message

--- UDC2017/js/script.js
+++ UDC2017/js/script.js
@@ -9,6 +9,6 @@ var map = L.map('map',{
 L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>'',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
     }
 ).addTo(map);
